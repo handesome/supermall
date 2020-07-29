cf8eeeef5f6f319808c347be7c464da79ede5ca9
handesome <56467838+handesome@users.noreply.github.com>
2020-07-29 09:36:14 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -4,7 +4,7 @@ export function request(config) {
 
   //创建axios实例
   const instance = axios.create({
-    baseURL: "http://152.136.185.210:8000/api/n3",
+    baseURL: "老师接口",
     timeout: 5000
   })
 
